02143b3be056761c6b1883ad484a37a02883ee61
Frank Delporte <frank@webtechie.be>
2020-10-30 07:23:29 +0000

    Adjusted Pi start scripts

--- MoleculeSampleApp/start_on_raspberrypi.sh
+++ MoleculeSampleApp/start_on_raspberrypi.sh
@@ -1,5 +1,7 @@
-sudo java -Dmonocle.platform.traceConfig=true \
-  -Dmonocle.platform=DRM \
+sudo java -Dmonocle.egl.lib=/opt/arm32fb-sdk/lib/libgluon_drm.so \
+  -Djava.library.path=/opt/arm32fb-sdk/lib \
+  -Dmonocle.platform.traceConfig=true \
+  -Dmonocle.platform=EGL \
   -Dprism.verbose=true \
   -Djavafx.verbose=true \
   -p /opt/arm32fb-sdk/lib \
