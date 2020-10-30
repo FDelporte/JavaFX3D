6fc20b4f4f555229cea6e17ad097a25294f051c3
Frank Delporte <frank@webtechie.be>
2020-10-30 07:26:04 +0000

    Adjusted Pi start scripts

--- 3DViewer/start_on_raspberrypi.sh
+++ 3DViewer/start_on_raspberrypi.sh
@@ -1,8 +1,10 @@
-sudo java -Dmonocle.platform.traceConfig=true \
-  -Dmonocle.platform=DRM \
+sudo java -Dmonocle.egl.lib=/opt/arm32fb-sdk/lib/libgluon_drm.so \
+  -Djava.library.path=/opt/arm32fb-sdk/lib \
+  -Dmonocle.platform.traceConfig=true \
+  -Dmonocle.platform=EGL \
   -Dprism.verbose=true \
   -Djavafx.verbose=true \
   -p /opt/arm32fb-sdk/lib \
   --add-modules javafx.controls,javafx.fxml \
   -jar target/Jfx3dViewerApp-0.0.1-jar-with-dependencies.jar \
-  /home/pi/JavaFX3D/Duke/Chess\ Duke/allStacked_solidColors_w0005_loweredPawnHat.ma
+  /home/pi/JavaFX3D/Duke/Chess_Duke/allStacked_solidColors_w0005_loweredPawnHat.ma
